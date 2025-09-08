979d3d31dda6657c128ef65fb6df9014f938ed4e
jose-baires <98169387+JoseOrtega17@users.noreply.github.com>
2025-09-08 16:09:13 -0600

    w

--- src/main.cpp
+++ src/main.cpp
@@ -31,28 +31,33 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(DATA0_PIN), ISR_data0, FALLING);
   attachInterrupt(digitalPinToInterrupt(DATA1_PIN), ISR_data1, FALLING);
 
+  pinMode(LED_BUILTIN, OUTPUT);  // LED interno
+
   memset((void*)bitBuffer, 0, sizeof(bitBuffer));
 }
 
 void loop() {
   static char localBuffer[27];
-  // static byte localLength = 0;
 
   noInterrupts();
   if (bitIndex == 26 && (millis() - lastBitTime > timeoutMs)) {
     strncpy(localBuffer, (const char*)bitBuffer, 26);
     localBuffer[26] = '\0';
-    // localLength = bitIndex;
     bitIndex = 0;
     interrupts();
 
     unsigned long raw = strtoul(localBuffer, NULL, 2);
-    // byte facilityCode = (raw >> 17) & 0xFF;
     unsigned int cardNumber = (raw >> 1) & 0xFFFF;
 
     // Enviar por RS232 al GPS
     Serial.print(cardNumber);
     Serial.print("\r\n");
+
+    // 🔴 Prende LED cuando hay un tag detectado
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(200);  // breve pulso
+    digitalWrite(LED_BUILTIN, LOW);
+
   } else {
     interrupts();
   }
